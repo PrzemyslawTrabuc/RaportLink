16404337305bda6d1956ecb8bf80f09e3989bedd
Unknown <czemek17@gmail.com>
2021-04-22 19:17:41 +0200

    Added event modal box (beta)

--- raport_building.php
+++ raport_building.php
@@ -11,35 +11,72 @@ if($_SESSION['uname']=="")
         <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.css">
         <link rel="stylesheet" href="@fortawesome\fontawesome-free\css\all.css">
         <link rel="stylesheet" href="custom_css\custom_css.css">  
-        <script src="node_modules\jquery\dist\jquery.min.js"></script>     
+        <script src="node_modules\jquery\dist\jquery.min.js"></script>  
+        <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>        
           <script>
-        $(document).ready(function() {
-       
-       $("button[name='add_event']").click(function() {
-           var domElement = $('<div class="event"><p>dasdasdasdsadssa</p></div>');
-           $("div[id='raport_header']").after(domElement);
+        $(document).ready(function test() {      
+       $("button[name='add_event']").click(function test() {
+        var text = document.getElementById("event").value;
+        var time = document.getElementById("time").value;
+           var domElement = $('<div class="time"><h3>'+time+'</h3>'+text+'</div>');           
+           $("div[id='raport_header']").before(domElement);        
+           document.getElementById("time").value="";
+           document.getElementById("event").value="";
        });
-       
+      
       });
-          </script>
+          </script>         
     </head>
     <body>      
      <div id="logo-top" class="container"><img src="images/Raport Link logo_light.svg"></div>
      <p class="welcome_text"><?php echo "Witaj ".$_SESSION['uname']  ?></p> 
      <p class="welcome_text"><?php echo "Tworzysz raport dla ".$_SESSION['uname']  ?></p> 
-     <div class="buttons_middle"><p class="buttons"><button name="add_event" class="btn btn-primary"><i class="fas fa-bolt fa-lg"></i> Dodaj zdarzenie</button>
+     <div class="buttons_middle"><button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-bolt fa-lg"></i> Dodaj zdarzenie</button>
       <br>
         <button type="button" class="btn btn-primary"><i class="far fa-image fa-lg"></i> Dodaj grafikę</button>
       <br>    
+      <br>
       <form action="scripts/logout.php">
-        <button type="submit" class="btn btn-danger"><i class="fas fa-sign-out-alt fa-lg"></i> Wyloguj</button></p>
+        <button type="submit" class="btn btn-danger"><i class="fas fa-sign-out-alt fa-lg"></i> Wyloguj</button>
       </form>
       <form action="scripts/logout.php">
-        <button type="submit" class="btn btn-info"><i class="fas fa-print fa-lg"></i> Drukuj</button></p>
+        <button type="submit" class="btn btn-info"><i class="fas fa-print fa-lg"></i> Drukuj</button>
       </form>
        </div>
        <div id="Raport">
          <div id="raport_header"></div>
-       </div>
+       </div>         
+</div> 
+</div>
+<!--------------------------------------------------------------------------------->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Czas:</label>
+            <input type="text" class="form-control" id="time">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Zdarzenie:</label>
+            <textarea class="form-control" id="event"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" name="add_event" class="btn btn-primary" data-dismiss="modal">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!--------------------------------------------------------------------------------->
     </body>
 </html>
