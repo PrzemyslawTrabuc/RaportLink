a0608fa19c8fdc74df74b4bc0059208d5c934c3c
Unknown <czemek17@gmail.com>
2021-04-28 14:48:59 +0200

    more logo fixes

--- signed_in.php
+++ signed_in.php
@@ -13,13 +13,13 @@ if($_SESSION['uname']=="")
         <link rel="stylesheet" href="custom_css\custom_css.css">        
     </head>
     <body>
-     <div id="logo-top" class="container"><img src="images/Raport Link logo_light.svg"></div>
+     <div id="logo-top" class="container"><img src="images/Raport_Link_logo_light.svg"></div>
      <p class="welcome_text"><?php echo "Witaj ".$_SESSION['uname']  ?></p> 
      <div class="buttons_middle">     
      <form method="POST" action="scripts/redirect_to_building.php">
       <button name="firma1" type="submit" class="btn btn-primary"><i class="far fa-building fa-lg"></i> Firma 1</button><img class="company_logo" src="images/test.jpg"></img>
         <br>
-        <button name="firma2" type="submit" class="btn btn-primary"><i class="far fa-building fa-lg"></i> Firma 2</button><img class="company_logo" src="images/Raport Link logo_light.svg"></img>
+        <button name="firma2" type="submit" class="btn btn-primary"><i class="far fa-building fa-lg"></i> Firma 2</button><img class="company_logo" src="images/Raport_Link_logo_light.svg"></img>
       <br>     
       </form>  
         <button type="button" class="btn btn-light"><i class="far fa-edit fa-lg"></i> Edytuj Raporty</button>     
