ce1693a1c85a19b5d5f5e1b6a23d438f6a345d83
StadnickiK <stakrzysztof@interia.pl>
2021-04-28 15:52:14 +0200

    Create session variables for firma and path to zdjecie

--- scripts/redirect_to_building.php
+++ scripts/redirect_to_building.php
@@ -5,17 +5,13 @@ session_start();
 foreach($_POST as $name => $content) { // Most people refer to $key => $value
     //echo "The HTML name: $name <br>";
     //echo "The content of it: $content <br>";
-    include "scripts/config.php";    
+    include "config.php";    
     $sql_query = "select * from firma where nrid = '$name'";
     $result = mysqli_query($link,$sql_query);
     $_SESSION['firma'] = $result;
 
-    $id = $row['nrid_zdjecie'];
-    $sql_query = "select * from zdjecie where nrid = '$id'";
-    $result = mysqli_query($link,$sql_query);
-    $row = mysqli_fetch_array($result);
-    $_SESSION['logo'] = $row['nazwa'];
+    $_SESSION['logo'] = $content;
  }
 // Redirect to login page
-//header("location: ../raport_building.php");
+header("location: ../raport_building.php");
 ?>
