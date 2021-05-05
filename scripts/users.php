5880c3a16a9b7b5e5a9653ebe380a4a34632506b
StadnickiK <stakrzysztof@interia.pl>
2021-05-05 14:25:19 +0200

    Add modal for user information editing

--- scripts/users.php
+++ scripts/users.php
@@ -3,6 +3,7 @@ session_start();
 if($_SESSION['uname']=="")
 {  
   header('Location: index.php');
+  exit;
 }
 ?>
 <!DOCTYPE html>
@@ -13,23 +14,35 @@ if($_SESSION['uname']=="")
         <link rel="stylesheet" href="..\custom_css\custom_css.css">
         <script src="..\node_modules\jquery\dist\jquery.min.js"></script>  
         <script src="..\node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+
+        <?php 
+          if(isset($_SESSION['name']))
+          {  
+            echo "<script type='text/javascript'>",
+            "function openModal() {".
+              "$('#oldUser').modal('show');".
+            "}",
+            "</script>";
+          }
+        ?>
+
     </head>
     <body>
         <div id="logo-top" class="container"><img src="../images/Raport_Link_logo_light.svg"></div>
         <p class="welcome_text"><?php echo "Witaj ".$_SESSION['uname']  ?></p> 
         <div class="buttons_middle">     
         <form action="redirect_to_users.php" method="POST">
-        <?php 
-            include "config.php";    
-            $sql_query = "select * from pracownicy";
-            $result = mysqli_query($link,$sql_query);
-            while($row = mysqli_fetch_array($result)){
-                $id = $row['nrid'];
-                $name = $row['Imie'];
-                $surname = $row['Nazwisko'];
-                echo "<button name=$id value=$id type='submit' class='btn btn-primary'><i class='far fa-building fa-lg'></i> $name $surname</button><br>";
-            } 
-        ?>
+          <?php 
+              include "config.php";    
+              $sql_query = "select * from pracownicy";
+              $result = mysqli_query($link,$sql_query);
+              while($row = mysqli_fetch_array($result)){
+                  $id = $row['nrid'];
+                  $name = $row['Imie'];
+                  $surname = $row['Nazwisko'];
+                  echo "<button name='id' value=$id type='submit' class='btn btn-primary'><i class='far fa-building fa-lg'></i> $name $surname</button><br>";
+              } 
+          ?>
         </form>
         <button class="btn btn-light" data-toggle="modal" data-target="#newUser"><i class="far fa-edit fa-lg"></i> Nowy użytkownik </button>
         <form action="redirect_to_signed_in.php" method="POST">
@@ -42,6 +55,41 @@ if($_SESSION['uname']=="")
 
 
 <!--------------------------------------------------------------------------------->
+<div class="modal fade" id="oldUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Nowy użytkownik</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="edit_user.php" method="POST">
+          <?php
+              $id = $_SESSION['id'];
+              $name = $_SESSION['name'];
+              $surname = $_SESSION['surname'];
+              echo "<input type='hidden' class='form-control' id='id' value='$id' name='id'>",
+              "<label for='name'>Imie: </label>",
+              "<input type='text' class='form-control' id='name' value='$name' name='name'>",
+              "<label for='surname'>Nazwisko: </label>",
+              "<input type='text' class='form-control' id='surname' value='$surname' name='surname'>",
+              "<label for='password'>Hasło:</label>",
+              "<input type='text' class='form-control' id='password' name='password'>",
+              "<button type='submit' class='btn btn-light'><i class='far fa-edit fa-lg'></i> Aktualizuj</button>";
+          ?> 
+        </form>
+      </div>
+      <div class="modal-footer">
+      <form action="redirect_to_users.php" method="POST">
+        <button type="submit" class="btn btn-secondary" data-dismiss="modal">Cofnij</button>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!--------------------------------------------------------------------------------->
 <div class="modal fade" id="newUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -52,7 +100,7 @@ if($_SESSION['uname']=="")
         </button>
       </div>
       <div class="modal-body">
-        <form action="create_user.php" method="POST">
+        <form action="edit_user.php" method="POST">
             <label for="name">Imie:</label>
             <input type="text" class="form-control" id="name" name="name" required>
             <label for="surname">Nazwisko:</label>
@@ -71,6 +119,15 @@ if($_SESSION['uname']=="")
   </div>
 </div>
 <!--------------------------------------------------------------------------------->
+
+        <?php 
+          if(isset($_SESSION['name']))
+          {  
+            echo "<script type='text/javascript'>",
+              "openModal();",
+            "</script>";
+          }
+        ?>
 
     </body>
 </html>
