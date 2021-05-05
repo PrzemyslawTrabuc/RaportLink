f8241658671ebe6cd8ebe3003762c47339b40b01
StadnickiK <stakrzysztof@interia.pl>
2021-05-05 14:19:57 +0200

    Add destruction of unnecesary session variables

--- scripts/redirect_to_users.php
+++ scripts/redirect_to_users.php
@@ -5,6 +5,42 @@ if($_SESSION['uname']=="")
 {  
   header('Location: index.php');
 }
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  if(isset($_POST['id'])){
+    $id = $_POST['id'];
+    if($id != NULL){
+      include "config.php"; 
+
+      $sql_query = "select * from pracownicy where nrid = '$id'";
+
+      $result = mysqli_query($link, $sql_query);
+      $row = mysqli_fetch_array($result);
+      
+      $_SESSION['id'] = $row['nrid'];
+      $_SESSION['name'] = $row['Imie'];
+      $_SESSION['surname'] = $row['Nazwisko'];
+
+      header("location: users.php");
+      exit;
+    }
+  }else{
+    if(isset($_SESSION['name'])){
+      unset($_SESSION['name']);
+    }
+    if(isset($_SESSION['surname'])){
+      unset($_SESSION['surname']);
+    }
+    if(isset($_SESSION['id'])){
+      unset($_SESSION['id']);
+    }
+    header("location: users.php");
+    exit;
+  }
+}else {
+  echo "Error...";
+}
+
 // Redirect to page
-header("location: users.php");
+
 ?>
