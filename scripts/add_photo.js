cb0e6ec0e42e3bc311254325995345b07f34d2ef
Unknown <czemek17@gmail.com>
2021-05-12 12:51:37 +0200

    fixed  empty image condition

--- scripts/add_photo.js
+++ scripts/add_photo.js
@@ -2,8 +2,9 @@ $(document).ready(function addPhoto() {
   $("button[name='add_photo']").click(function addPhoto() {
    var text_photo = document.getElementById("event_photo").value;
    var time_photo = document.getElementById("time_photo").value;  
-   var img = document.getElementById("imageResult").src;   
-   if(time_photo != "" && text_photo !="" && img != "http://localhost/Agile/RaportLink/raport_building.php#" )
+   var img = document.getElementById("imageResult").src; 
+   console.log(img); 
+   if(time_photo != "" && text_photo !="" && img.includes("/Agile/RaportLink/raport_building.php#") != true)
    {    
       var domElement = $('<div class="event"><h3><i class="far fa-clock"></i> '+time_photo+'</h3><br>'+text_photo+'</div><div class="raport_img"><img src='+img+'><br></div><hr>');           
       $("div[id='Raport_footer']").before(domElement);        
