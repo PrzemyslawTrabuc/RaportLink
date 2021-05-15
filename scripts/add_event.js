558323d2177cb6595542f8d1db56a3a7c0ada976
Unknown <czemek17@gmail.com>
2021-05-15 15:31:30 +0200

    css fixes

--- scripts/add_event.js
+++ scripts/add_event.js
@@ -6,7 +6,7 @@ $(document).ready(function addEvent() {
    {
       var time_stamp = new Date();
       var date = time_stamp.getFullYear()+'-'+(time_stamp.getMonth()+1)+'-'+time_stamp.getDate()+' '+time_stamp.getHours()+':'+time_stamp.getMinutes()+':'+time_stamp.getSeconds();
-      var domElement = $('<div class="event"><h3><i class="far fa-clock"></i> '+time+'</h3><br><p>'+text+'<p class="time_stamp">'+date+' <button type="button" name="delete_event" class="btn btn-danger" onclick="return this.parentNode.parentNode.remove();">Usuń</button></p></div>');           
+      var domElement = $('<div class="event"><h3><i class="far fa-clock"></i> '+time+'</h3><br><p>'+text+'<p class="time_stamp">'+date+' <button type="button" name="delete_event" id="del_btn" class="btn btn-danger" onclick="return this.parentNode.parentNode.remove();"><i class="fas fa-trash fa-lg"></i></button></p></div>');           
       $("div[id='Raport_footer']").before(domElement);        
       document.getElementById("time").value="";
       document.getElementById("event").value="";
