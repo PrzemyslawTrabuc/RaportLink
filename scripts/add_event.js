76c5c10ad03d4f687eaea8f4945a454ce335dd45
StadnickiK <stakrzysztof@interia.pl>
2021-05-27 10:57:03 +0200

    Add json parse for user input in raport_building

--- scripts/add_event.js
+++ scripts/add_event.js
@@ -6,13 +6,13 @@ $(document).ready(function addEvent() {
    {
       var time_stamp = new Date();
       var date = time_stamp.getFullYear()+'-'+(time_stamp.getMonth()+1)+'-'+time_stamp.getDate()+' '+time_stamp.getHours()+':'+time_stamp.getMinutes()+':'+time_stamp.getSeconds();
-      var domElement = $('<div class="event"><h3><i class="far fa-clock"></i> '+time+'</h3><br><p>'+text+'<p class="time_stamp">'+date+' <button type="button" name="delete_event" id="del_btn" class="btn btn-danger" onclick="return this.parentNode.parentNode.remove();"><i class="fas fa-trash fa-lg"></i></button></p></div>');           
+      var domElement = $('<div class="event"><h3><i class="far fa-clock"></i> '+time+'</h3><br><p>'+text+'<p class="time_stamp">'+date+'<button type="button" name="delete_event" id="del_btn" class="btn btn-danger" onclick="removeElement(this)"><i class="fas fa-trash fa-lg"></i></button></p></div>');    
       $("div[id='Raport_footer']").before(domElement);        
       document.getElementById("time").value="";
       document.getElementById("event").value="";
       document.getElementById('alert').style.display = 'none';  
       var raportHTML = document.getElementById("Raport");
-      var raport = raportHTML.innerHTML;
+      var raport = JSON.stringify(raportHTML.innerHTML);
       sessionStorage.setItem('raport', raport);
    }else
    {
@@ -21,3 +21,17 @@ $(document).ready(function addEvent() {
    }
   });
  });
+
+ window.addEventListener("load", () => {
+   function newRaport(){
+      if(sessionStorage && sessionStorage.getItem("raport"))
+         if (confirm("Jesteś pewny? Utracisz bezpowrotnie niedokończony raport")) {
+            sessionStorage.removeItem("raport");
+            window.location.reload();
+         } 
+   }
+   document.getElementById("new_raport").addEventListener('click', newRaport);
+ });
+//  window.onbeforeunload = function (e) {
+//    sessionStorage.removeItem("raport");
+//  };
--- scripts/add_photo.js
+++ scripts/add_photo.js
@@ -8,7 +8,7 @@ $(document).ready(function addPhoto() {
    {   
       var time_stamp = new Date(); 
       var date = time_stamp.getFullYear()+'-'+(time_stamp.getMonth()+1)+'-'+time_stamp.getDate()+' '+time_stamp.getHours()+':'+time_stamp.getMinutes()+':'+time_stamp.getSeconds();
-      var domElement = $('<div class="event"><h3><i class="far fa-clock"></i> '+time_photo+'</h3><br>'+text_photo+'<div class="raport_img"><img src='+img+'></img><p class="time_stamp">'+date+'<button type="button" name="delete_event" id="del_btn" class="btn btn-danger" onclick="return this.parentNode.parentNode.parentNode.remove();"><i class="fas fa-trash fa-lg"></i></button></p></div>');           
+      var domElement = $('<div class="event"><h3><i class="far fa-clock"></i> '+time_photo+'</h3><br>'+text_photo+'<div class="raport_img"><img src='+img+'></img><p class="time_stamp">'+date+'<button type="button" name="delete_event" id="del_btn" class="btn btn-danger" onclick="removeElement(this)"><i class="fas fa-trash fa-lg"></i></button></p></div>');           
       $("div[id='Raport_footer']").before(domElement);        
       document.getElementById("time_photo").value="";
       document.getElementById("event_photo").value="";   
@@ -16,7 +16,7 @@ $(document).ready(function addPhoto() {
       document.getElementById('open-editor').hidden = 1;
       img = document.getElementById("imageResult").src = "";       
       var raportHTML = document.getElementById("Raport");
-      var raport = raportHTML.innerHTML;
+      var raport = JSON.stringify(raportHTML.innerHTML);
       sessionStorage.setItem('raport', raport);
    }else
    {
